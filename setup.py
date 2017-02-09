25f7ce74532c8c7ed5aa08da128987037b67a990
Kyle Lahnakoski <klahnakoski@mozilla.com>
2017-02-09 11:43:11 -0500

    updates from other projects

--- setup.py
+++ setup.py
@@ -44,7 +44,7 @@ def find_packages(where='.', lib_prefix='', exclude=()):
 
 setup(
     name='mo-json-config',
-    version="1.0.17039",
+    version="1.0.17041",
     description='More JSON Configuration! JSON configuration files with `$ref` and template overlays',
     long_description=long_desc,
     author='Kyle Lahnakoski',
