c1678441afc98fca189164c371730581187e64bf
Kyle Lahnakoski <kyle@lahnakoski.com>
2023-01-03 23:39:07 -0500

    fix coverage

--- mo_json_config/configuration.py
+++ mo_json_config/configuration.py
@@ -1,20 +1,29 @@
-import re
-
-from mo_dots import is_data, to_data, join_field, Data
+from mo_dots import is_data, to_data, join_field, leaves_to_data
 from mo_dots.datas import register_data
-from mo_logs.strings import strip, wordify
+from mo_logs import logger
+from mo_logs.strings import wordify
 
 
 class Configuration:
 
-    def __init__(self, lookup):
-        self.lookup = to_data(lookup)
+    def __init__(self, config):
+        if not is_data(config):
+            logger.error("Expecting data, not {{config}}", config=config)
+        self.lookup = leaves_to_data({join_field(wordify(path)): value for path, value in to_data(config).leaves()})
 
-    def __ior__(self, more_config):
-        if is_data(more_config):
-            for path, value in to_data(more_config).leaves():
-                clean_path = join_field(wordify(path))
-                self.lookup[clean_path]=value
+    def __iadd__(self, other):
+        """
+        RECURSIVE ACCUMULATION OF PROPERTIES
+        """
+        self.lookup += Configuration(other).lookup
+        return self
+
+    def __ior__(self, other):
+        """
+        RECURSIVE COALESCE OF PROPERTIES
+        """
+        self.lookup |= Configuration(other).lookup
+        return self
 
     def __getattr__(self, item):
         clean_path = join_field(wordify(item))
