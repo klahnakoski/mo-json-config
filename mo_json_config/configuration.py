0957836c1cc99b804022e4e296fcc63bc3a0524d
Kyle Lahnakoski <kyle@lahnakoski.com>
2023-01-04 23:39:21 -0500

    fixes

--- mo_json_config/configuration.py
+++ mo_json_config/configuration.py
@@ -1,5 +1,14 @@
-from collections import Mapping
+# encoding: utf-8
+#
+#
+# This Source Code Form is subject to the terms of the Mozilla Public
+# License, v. 2.0. If a copy of the MPL was not distributed with this file,
+# You can obtain one at http://mozilla.org/MPL/2.0/.
+#
+# Contact: Kyle Lahnakoski (kyle@lahnakoski.com)
+#
 
+from mo_future import Mapping
 from mo_dots import is_data, join_field, leaves_to_data, concat_field
 from mo_dots.datas import register_data, Data
 from mo_logs import logger
