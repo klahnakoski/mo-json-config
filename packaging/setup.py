7f0c1f78ae1bdb11f593eeb30c53349fceeaeb3c
Kyle Lahnakoski <kyle@lahnakoski.com>
2024-01-13 04:48:36 -0500

    updates from other projects

--- packaging/setup.py
+++ packaging/setup.py
@@ -14,5 +14,5 @@ setup(
     name='mo-json-config',
     packages=["mo_json_config"],
     url='https://github.com/klahnakoski/mo-json-config',
-    version='4.483.24007'
+    version='4.484.24013'
 )
